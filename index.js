a941a0f1bfed3fb3579f03875f41206686bc4dde
Ajay <ajay.ip3105@gmail.com>
2022-04-07 16:31:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,92 @@
-function forEach() {
-
+function forEach(array, callback){
+  for(let i = 0 ; i<array.length; i++) {
+    callback(array[i], i, array)
+  }
+}
+  
+const array = [1, 2, 3]
+const fun = function(num) {
+  console.log(num)
 }
 
-function map() {
-
+function map(array, callback) {
+  const newArray = []
+  for (let i = 0; i < array.length; i++){
+    newArray.push(callback(array[i]))
+  }
+  return newArray
 }
 
-function filter() {
-
+function filter(ary, callback) {
+  const newArr = []
+  for (let i = 0; i < ary.length; i++){
+    if (callback(ary[i]) === true){
+      newArr.push(ary[i])
+    }
+  }
+  return newArr
 }
 
-function every() {
-
+function every(ary, callback) {
+  for(let el of ary){
+    if(!callback(el)){
+      return false
+    }
+  }
+  return true
 }
 
-function some() {
-
+function some(ary, callback) {
+  for(let el of ary){
+    if(callback(el)){
+      return true
+    }
+  }
+  return false
 }
 
-function majority() {
-
+function majority(ary, calback) {
+  let trueCount =0
+  for (let i = 0; i < ary.length; i++){
+    if(calback(ary[i])){
+      trueCount++
+      if(trueCount>ary.length/2){
+        return true
+      }
+    }
+  }
+  return false
 }
 
-function once() {
-
+function once(calback) {
+  let isCalled =false
+  let value;
+  return function(args){
+    if(isCalled)return value
+    isCalled = true
+    value = calback(args)
+    return value
+  }
 }
 
-function groupBy() {
-
+function groupBy(ary, calback) {
+  const newObj = {}
+  for (let el of ary) {
+    if (newObj[calback(el)]){
+      newObj[calback(el)].push(el)
+    } else {
+    newObj [calback(el)] = [el]
+    }
+  }
+return newObj
 }
 
-function arrayToObject() {
-
+function arrayToObject(ary, calback) {
+  const newObj = {}
+  for(let el of ary){
+    newObj[el] = calback(el)
+  }
+  return newObj
 }
 
 module.exports = { 
